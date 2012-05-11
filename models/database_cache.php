82d0bc2c030beadbef08cf2ef0204010cb2ea383
alan bount <alan@zeroasterisk.com>
2012-05-11 18:14:13 -0400

    added database caching for cakephp 1.3

---
+++ models/database_cache.php
@@ -0,0 +1,66 @@
+<?php
+class DatabaseCache extends IcingAppModel {
+	public $name = 'DatabaseCache';
+	public $primaryKey = 'key';
+	public $useTable = 'database_caches';
+	
+	/**
+	* Find the value by key
+	* @param string key
+	* @return mixed value
+	*/
+	function findValueByKey($key){
+		$retval = $this->find('first', array(
+			'conditions' => array(
+				'DatabaseCache.key' => $key,
+				'DatabaseCache.duration >=' => time() 
+			)
+		));
+		if (!empty($retval)) {
+			return json_decode($retval[$this->alias]['value'], true);
+		}
+		return false;
+	}
+	
+	/**
+	* Write the value to the database
+	* @param string key
+	* @param mixed value
+	* @param mixed duration (strtotime friendly)
+	* @return boolean success
+	*/
+	function writeValueByKey($key, $value, $duration){
+		$this->create(false);
+		$save_data = array(
+			'key' => $key,
+			'value' => json_encode($value),
+			'duration' => time() + $duration
+		);
+		return !!$this->save($save_data);
+	}
+	
+	/**
+	* Delete a record by it's key
+	* @param string key
+	* @return boolean success
+	*/
+	function deleteByKey($key){
+		return $this->delete(array($this->alias.'.key LIKE' => str_replace('%%', '%', '%'.$key)));
+	}
+	
+	/**
+	* Clear all records
+	* @return boolean success
+	*/
+	function clearAll(){
+		return $this->deleteAll(array('1=1'));
+	}
+	
+	/**
+	* Clear the expired cache keys
+	* @return boolean success
+	*/
+	function clearExpired(){
+		return $this->deleteAll(array('DatabaseCache.duration <' => time()));
+	}
+}

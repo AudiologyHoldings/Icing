3e9229e6ea9dbffbaac478b4f687e1021909b8a4
Nick Baker <nick@webtechnick.com>
2012-09-06 18:40:31 -0600

    Adding function to return upload limit.

--- Lib/IcingUtil.php
+++ Lib/IcingUtil.php
@@ -1,5 +1,11 @@
 <?php
 class IcingUtil extends Object {
+	static function uploadLimit(){
+		$max_upload = (int)(ini_get('upload_max_filesize'));
+		$max_post = (int)(ini_get('post_max_size'));
+		$memory_limit = (int)(ini_get('memory_limit'));
+		return min($max_upload, $max_post, $memory_limit);
+	}
 	/**
 	* this function takes any number as input and returns a padded/grouped result
 	* 	eg: 666 -> 00600/00666

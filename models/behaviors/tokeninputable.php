8d3e172e50a4857951b03e0c8b28489f7d513231
alan bount <alan@zeroasterisk.com>
2013-08-28 14:35:31 -0400

    Bug in TokeninputableBehavior, validation+saveAll
    saveAll() does run beforeValidate() but alterations to $Model->data don't propagate to the saveAll() function
    
    So hasMany nodes which are cleaned up via the callback, and removed, are still passed into the saveAll() function
    and thus fail at validation steps (assuming validation exists)
    
    The current approach extracts those hasManyNodes from $Model->data early on, stashes it, and runs it in afterSave()
    
    this is very similar to the prior functionality except that it is a stand alone function
    `dataCleanupTokenInput()` which is called via `beforeValidate()` and `beforeSave()` but can (and should) be called manually
    from the controller/model before we submit our `saveAll()` call.
    
        $this->create(false);
        $this->set($data);
        // Tokeninputable needs to modify $Model->data, but beforeValidate
        // doesn't let it happen "fast enough" (also sets to $Model->data)
        $data = $this->dataCleanupTokenInput($data);
        $result = $this->saveAll($data);

--- models/behaviors/tokeninputable.php
+++ models/behaviors/tokeninputable.php
@@ -22,6 +22,13 @@ class TokeninputableBehavior extends ModelBehavior {
 	public $settings = array();
 
 	/**
+	 * temp data placeholder, for data extracted for custom save functionality
+	 *
+	 * @var array
+	 */
+	public $data = array();
+
+	/**
 	 * Default settings
 	 *
 	 * @var string
@@ -42,31 +49,73 @@ class TokeninputableBehavior extends ModelBehavior {
 	}
 
 	/**
-	 * Before Save Callback
+	 * Reformat data on model, as needed for this method
 	 *
-	 * reformat data as needed, so saveAll() can handle it as native code
+	 * NOTE: Needs to happen before Validate Callback
+	 *   validation callback doesn't allow us to alter the data passed into saveAll() {in CakePHP 1.3}
+	 *   therefore, we need to call like this:
 	 *
-	 * HABTM only
-	 * hasMany done in afterSave on a custom functionality
+	 *    $this->create(false);
+	 *    $this->set($data);
+	 *    // Tokeninputable needs to modify $Model->data, but beforeValidate
+	 *    // doesn't let it happen "fast enough" (also sets to $Model->data)
+	 *    $data = $this->dataCleanupTokenInput($data);
+	 *    $result = $this->saveAll($data);
+	 *
+	 *
+	 *
+	 * reformat data as needed,
+	 *   extract any data to be saved via TokeninputableBehavior from $Model->data
+	 *   so validate() will pass (in the case of nesting and reformatting)
+	 *   so saveAll() can handle it as native code
+	 *
+	 * HABTM = reformatting
+	 * hasMany = extracted ~ done in afterSave on a custom functionality
 	 *
 	 * @param appmodel $model model instance
+	 * @param mixed $data optionally pass in $data here, sets to model
 	 * @return boolean true to continue, false to abort the save
 	 * @access public
 	 */
-	public function beforeSave($Model) {
+	public function dataCleanupTokenInput($Model, $data=null) {
 		#extract($this->settings[$Model->alias]);
-		foreach ($Model->hasAndBelongsToMany as $assocName => $assocConf) {
-			$saved = $this->prepHABTM($Model, $assocName, $assocConf);
+		if (!empty($data)) {
+			$Model->set($data);
 		}
+		foreach ($Model->hasMany as $assocName => $assocConf) {
+			$this->prepHasMany($Model, $assocName, $assocConf);
+		}
+		foreach ($Model->hasAndBelongsToMany as $assocName => $assocConf) {
+			$this->prepHABTM($Model, $assocName, $assocConf);
+		}
+		return $Model->data;
 	}
 
 	/**
-	 * Before save method. Called before all saves
+	 * Before Validate callback, ensures dataCleanupTokenInput is called
 	 *
-	 * Overriden to transparently translate any association fields name->id
+	 * @return boolean
+	 */
+	public function beforeValidate($Model) {
+		$this->dataCleanupTokenInput($Model);
+		return true;
+	}
+
+	/**
+	 * Before Save callback, ensures dataCleanupTokenInput is called
 	 *
-	 * hasMany only
-	 * HABTM done in beforeSave + saveAll
+	 * @return boolean
+	 */
+	public function beforeSave($Model) {
+		$this->dataCleanupTokenInput($Model);
+		return true;
+	}
+
+	/**
+	 * After Save callback,
+	 * actually saves all hasMany nodes, stored in $this->data
+	 *
+	 * NOTE: HABTM done in normal saveAll, reformatted in prepHABTM() & dataCleanupTokenInput()
 	 *
 	 * @param appmodel $model model instance
 	 * @return boolean true to continue, false to abort the save
@@ -99,15 +148,20 @@ class TokeninputableBehavior extends ModelBehavior {
 	 * @return boolean
 	 */
 	public function saveHasMany($Model, $assocName, $foreignKey) {
-		if (empty($Model->data[$assocName][$assocName])) {
+		// data should have already been extracted to this storage path
+		if (empty($this->data[$Model->alias]['hasManyNodes'][$assocName])) {
 			// no value for this association
 			return false;
 		}
-		$data = $Model->data[$assocName][$assocName];
+		// extract data from the storage container on this Behaviour
+		$data = $this->data[$Model->alias]['hasManyNodes'][$assocName];
+		// reset and clear hasManyNodes
+		unset($this->data[$Model->alias]['hasManyNodes'][$assocName]);
 		if (!is_string($data)) {
 			// not a CSV string, which is what tokeninput would give you
 			return false;
 		}
+		$assocPK = $Model->{$assocName}->primaryKey;
 		$thisId = $Model->id;
 		$submittedIds = array_unique(explode(',', trim($data)));
 		// remove "thisId" from all records
@@ -118,8 +172,9 @@ class TokeninputableBehavior extends ModelBehavior {
 		// now add "thisId" to all the tokenInput records
 		$added = $Model->{$assocName}->updateAll(
 			array("{$assocName}.{$foreignKey}" => '"' . $thisId  . '"'),
-			array("{$assocName}.{$foreignKey}" => $submittedIds)
+			array("{$assocName}.{$assocPK}" => $submittedIds)
 		);
+		//debug(compact('removed', 'added', 'thisId', 'submittedIds', 'assocName', 'foreignKey', 'assocPK'));die();
 		return true;
 	}
 
@@ -135,6 +190,10 @@ class TokeninputableBehavior extends ModelBehavior {
 	 * NOTE: if you do a save, not a saveAll, this wouldn't work, but it also
 	 * wont hurt anything.
 	 *
+	 * @param AppModel $Model Model instance
+	 * @param string $assocName (model name/alias)
+	 * @param array $assocConf
+	 * @return boolean
 	 */
 	public function prepHABTM($Model, $assocName) {
 		if (empty($Model->data[$assocName][$assocName])) {
@@ -150,6 +209,34 @@ class TokeninputableBehavior extends ModelBehavior {
 		$submittedIds = array_unique(explode(',', trim($data)));
 		// just set the data back into the expected format
 		$Model->data[$assocName][$assocName] = $submittedIds;
+		return true;
+	}
+
+	/**
+	 * HasMany data needs to be extracted, removed from the Model->data
+	 * and stored for processing in afterSave()
+	 *
+	 * SaveAll doesn't validate these hasOne nodes...
+	 *
+	 * @param AppModel $Model Model instance
+	 * @param string $assocName (model name/alias)
+	 * @param array $assocConf
+	 * @return boolean
+	 */
+	public function prepHasMany($Model, $assocName, $assocConf) {
+		if (empty($Model->data[$assocName][$assocName])) {
+			// no value for this association
+			return false;
+		}
+		// set this data for later use
+		$this->data[$Model->alias]['hasManyNodes'][$assocName] = $Model->data[$assocName][$assocName];
+		// unset this data from Model->data
+		unset($Model->data[$assocName][$assocName]);
+		// clean up, if this is now empty, delete completly
+		if (empty($Model->data[$assocName])) {
+			unset($Model->data[$assocName]);
+		}
+		// done
 		return true;
 	}
 

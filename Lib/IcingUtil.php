be394435793eccf95429614d87602290839d848f
Nick Baker <nick@webtechnick.com>
2013-09-16 15:24:53 -0600

    adding isHttps to Icing

--- Lib/IcingUtil.php
+++ Lib/IcingUtil.php
@@ -54,6 +54,16 @@ class IcingUtil extends Object {
 		return min($max_upload, $max_post, $memory_limit);
 	}
 	/**
+	* Returns if request is HTTPS or not.
+	* @return boolean true if HTTPS, false if not.
+	*/
+	public static function isHttps(){
+		return (
+			(isset($_SERVER['HTTP_X_REMOTE_PROTOCOL']) && !empty($_SERVER['HTTP_X_REMOTE_PROTOCOL'])) ||
+			(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on') ||
+			(isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT']==443));
+	}
+	/**
 	* this function takes any number as input and returns a padded/grouped result
 	* 	eg: 666 -> 00600/00666
 	* 	eg: abcdefg -> abcde00/abcdefg

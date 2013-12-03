8f639144bc737265b7fc7377e1f5309e8a137ef4
Nick Baker <nick@webtechnick.com>
2013-12-03 12:49:56 -0700

    Updating shcmea with new index on created for faster retreival.

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -11,27 +11,29 @@ class IcingSchema extends CakeSchema {
 	public $database_caches = array(
 		'key' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'value' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'duration' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'duration' => array('type' => 'integer', 'null' => false, 'default' => '0'),
 		'indexes' => array(
 			'key' => array('column' => 'key', 'unique' => 1)
 		),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MEMORY')
 	);
+
 	public $icing_versions = array(
 		'id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'model_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
 		'model' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'json' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'json' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'key' => 'index'),
 		'url' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'ip' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'is_delete' => array('type' => 'boolean', 'null' => false, 'default' => null),
-		'is_minor_version' => array('type' => 'boolean', 'null' => false, 'default' => null, 'comment' => 'true if saved within x timeframe of previous version'),
+		'is_delete' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
-			'model_id' => array('column' => array('model_id', 'model'), 'unique' => 0)
+			'model_id' => array('column' => array('model_id', 'model'), 'unique' => 0),
+			'created' => array('column' => 'created', 'unique' => 0)
 		),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
 	);
+
 }

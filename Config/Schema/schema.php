7fe6d2bdcdc76ecb056bb6a5bac860ff5c938920
Nick Baker <nick@webtechnick.com>
2014-04-03 09:57:15 -0600

    Updating schema for install. better indexes.

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -21,19 +21,22 @@ class IcingSchema extends CakeSchema {
 	public $icing_versions = array(
 		'id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'model_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'user_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
 		'model' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'json' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'key' => 'index'),
 		'url' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'ip' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'is_delete' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'is_minor_version' => array('type' => 'boolean', 'null' => false, 'default' => null, 'key' => 'index', 'comment' => 'true if saved within x timeframe of previous version'),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
 			'model_id' => array('column' => array('model_id', 'model'), 'unique' => 0),
 			'created' => array('column' => 'created', 'unique' => 0),
 			'model' => array('column' => 'model', 'unique' => 0),
-			'model_id_2' => array('column' => 'model_id', 'unique' => 0)
+			'model_id_2' => array('column' => 'model_id', 'unique' => 0),
+			'user_id' => array('column' => 'user_id', 'unique' => 0),
+			'is_minor_version' => array('column' => 'is_minor_version', 'unique' => 0),
 		),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
